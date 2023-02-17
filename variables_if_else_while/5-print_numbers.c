0536c270448250bfc30218aa7ec6c8e335f23397
binbashz <5732@holbertonstudnets.com>
2023-02-17 11:07:36 -0800

    5-print-numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * main - print numbers
+ * of base 10 starting from 0,
+ * followed by a new line
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+	int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+
+	printf("%d", numbers);
+
+		printf("\n");
+
+	return (0);
+}
