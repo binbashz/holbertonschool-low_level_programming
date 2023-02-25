54d5c0530b9f9e46601e7a9c158285a5eed55a8b
binbashz <5732@holbertonstudnets.com>
2023-02-24 18:19:45 -0800

    10-print-triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,14 +8,26 @@
 
 void print_triangle(int size)
 {
-	int m, c;
+        int m, c;
 
-	if (size > 0)
-	{
-		for (c = 1 ; c <= size ; c++)
-		{
-			for (m = 1 ; m <= (size - c) ; m++)
+        if (size > 0)
+        {
+                for (c = 1 ; c <= size ; c++)
+                {
+                        for (m = 1 ; m <= (size - c) ; m++)
+                        {
+                                _putchar(' ');
+                        }
+
+
+			for (m = 0 ; m < c ; m++)
 			{
-				_putchar(' ');
+				_putchar('#');
 			}
+			_putchar('\n');
+		}
+	}
 
+	else
+		_putchar('\n');
+}
