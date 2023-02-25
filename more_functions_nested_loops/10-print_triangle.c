fe4da21d1a75f4a65a3540c0d611f8ac24e888f2
binbashz <5732@holbertonstudnets.com>
2023-02-24 18:33:31 -0800

    print-triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,35 @@
-#include "main.h"
 
 /**
- * print_triangle  Prints a triangle
+ * print_triangle - Prints a triangle
  * followed by a new line
  * @size: Size of the triangle
 */
 
+
+#include "main.h"
+
+
 void print_triangle(int size)
 {
-        int m, c;
+	int m, c;
 
-        if (size > 0)
-        {
-                for (c = 1 ; c <= size ; c++)
-                {
-                        for (m = 1 ; m <= (size - c) ; m++)
-                        {
-                                _putchar(' ');
-                        }
+	if (size > 0)
+{
+	for (c = 1 ; c <= size ; c++)
+	{
+	for (m = 1 ; m <= (size - c) ; m++)
+{
+	_putchar(' ');
+}
 
-
-			for (m = 0 ; m < c ; m++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+	for (m = 0 ; m < c ; m++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+	}
 	}
 
 	else
-		_putchar('\n');
+	_putchar('\n');
 }
