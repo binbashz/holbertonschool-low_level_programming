55bb18443a0e751be0d7d44c4b173ed4ed72fcee
binbashz <5732@holbertonstudnets.com>
2023-02-24 18:03:28 -0800

    fizz-buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
  * main - Prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three print Fizz instead of the number and
@@ -16,3 +16,22 @@ int main(void)
 	{
 		if ((fbz % 3) == 0 && (fbz % 5) == 0)
 			printf("FizzBuzz");
+
+		else if ((fbz % 3) == 0)
+			printf("Fizz");
+
+		else if ((fbz % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", fbz);
+
+		if (fbz == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
