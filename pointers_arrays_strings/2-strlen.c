f2ffe309c78ec3d599caa6d972ae2164bacccbae
binbashz <5732@holbertonstudnets.com>
2023-02-27 08:24:33 -0800

    2-strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function with 1 argument
+ * @s: char type pointer
+ *
+ * Description: strlen : function to return Length of string
+ * Return: String
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
