a264a9725359e9f0584cc263e0df7af60ab46acc
binbashz <5732@holbertonstudnets.com>
2023-02-27 08:05:59 -0800

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function with two int type pointer argument
+ * @a: 1 argument type int
+ * @b: 2 argument type int 
+ *
+ * Description: Using pointer to swap a and b 
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int horses;
+
+	horses = *a;
+	*a = *b;
+	*b = horses;
+}
