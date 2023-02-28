370dd456295b1a2aa47b9e2488fa7c6fbf976065
binbashz <5732@holbertonstudnets.com>
2023-02-27 16:58:07 -0800

    4-print--i

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,23 @@
+/**
+*  print_rev -  Takes a string as input,
+*  prints it in reverse order and appends a new line to the end
+*
+*  @s: caracter string
+*
+*/
 #include "main.h"
+#include <stdio.h>
 #include <string.h> /*  - string.h - header file for operations with strings */
 
-
-
-	void print_rev(char *s) 
+void print_rev(char *s)
 {
-	int lengthwise = strlen(s);
+	int star = 0;
 
-	for (int i = lengthwise -1; i>= 0; i--)
-	{
-	putchar(s[i});
+	for (; s[star]; star++)
+
+	star -= 1;
+
+	for (; star > -1; star--)
+		printf("%c", s[star]);
+	putchar('\n');
 }
-
-	
-
