66fbc7ccdbd936796cb4db1367b7020645f5aa87
binbashz <5732@holbertonstudnets.com>
2023-02-28 05:23:55 -0800

    9-strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+/*trcpy - copies the string pointed to by src,
+ * including null byte (\0), to the buffer pointed
+ * to by dest.
+ *
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j  = 0;
+
+	
+	while (ptr[i] != '\0')
+	{
+		
+		dest[j] = ptr[j];
+		j++;
+	}
+
+	
+
+	return (0);
+}
