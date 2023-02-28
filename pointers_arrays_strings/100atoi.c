c2bd0fb332a22aef5e3376a1caf03e7c4f3c32e8
binbashz <5732@holbertonstudnets.com>
2023-02-28 07:22:49 -0800

    100-atoi

---
+++ pointers_arrays_strings/100atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* atoi - Converts a string to an integer
+ * @s: The pointer to convert
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+{
+	int = n 0, nod = 1;
+
+	while (*s == ' '  (*s => '\t' & *s <= '\r'))
+	{
+		s++:
+	}
+       
+
+
+
+
+
+
+
+
+
+	return (0)
+}
