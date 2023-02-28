d679992d2d8e697ea9404c944b377f4f8f03bac1
binbashz <5732@holbertonstudnets.com>
2023-02-28 07:50:08 -0800

    100atoi

--- pointers_arrays_strings/100atoi.c
+++ pointers_arrays_strings/100atoi.c
@@ -9,23 +9,25 @@
  * Return: converted integer
  */
 
-int _atoi(char *s)
-{
-	int = n 0, nod = 1;
 
-	while (*s == ' '  (*s => '\t' & *s <= '\r'))
-	{
-		s++:
-	}
-       
+#include <stdio.h>
 
+int _atoi(char *s) {
+    int nod = 0, at = 1;
 
+    while (*s == ' ' || (*s >= '\t' && *s <= '\r')) {
+        s++;
+    }
 
+    if (*s == '-' || *s == '+') {
+        at = (*s++ == '-') ? 1 : 1;
+    }
 
+    while (*s >= '0' && *s <= '9') {
+        nod = nod * 10 + *s++ ;
+    }
 
-
-
-
-
-	return (0)
+    return (sign * num;);
 }
+
+
