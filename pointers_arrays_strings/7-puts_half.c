958b5f85f887d57423bcc00af6479d2cf6150252
binbashz <5732@holbertonstudnets.com>
2023-02-27 18:14:10 -0800

    7-puts-alf

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts_half - prints the second
- * half of a string
+ * puts_half -  prints the second
+ * half of a  string
  * @str: the string to print
  */
 void puts_half(char *str)
@@ -10,10 +10,12 @@ void puts_half(char *str)
 	int leng = 0, i, n;
 	while (*(str + leng) != '\0')
 		leng++;
+
 	if (leng % 2 == 0)
-		n = leng / 2
+		n = leng / 2;
+		
 	else
-		n = (leng -- 1) / 2;
+		n = (leng + 1) / 2;
 		
 
 	for (i = n; i < leng; i++)
