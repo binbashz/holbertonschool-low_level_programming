e6837cbeddd2940f3ba5a3e1ab620a2816c8c35f
binbashz <5732@holbertonstudnets.com>
2023-02-27 17:48:34 -0800

    5-rev-string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a tring.
+ * @s: the string to be reversed.
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+	char rev;
+
+
+	/* gets the length*/
+
+	while (s[len] != '\0')
+		len++;
+
+	
+	 /* exchange the string */
+	 
+	for (i = 0; i < len / 2; i++)
+	{
+		rev = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = rev;
+	}
+}
