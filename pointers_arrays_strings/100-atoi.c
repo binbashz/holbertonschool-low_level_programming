bf7b176d2729cef50d7d112566b39983339fc303
binbashz <5732@holbertonstudnets.com>
2023-02-28 09:28:07 -0800

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,46 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer
  * @s: The pointer to convert
- * Return: converted integer
- */
-
+ * Return: converted int
+**/
 
 
 	int _atoi(char *s)
 {
-	int nod = 0, at = 1;
+	int re = 0;
+	int sign = 1;
+	int number;
 
-	while (*s == ' ' || (*s >= '\t' && *s <= '\r'))
-{
-	s++;
-}
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+		sign *= -1;
+		}
+		else if (*s == '+')
+		{
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			number = *s - '0';
 
-	if (*s == '-' || *s == '+')
-{
+		if (re > (INT_MAX - number) / 10)
+		{
+			return (sign > 0 ? INT_MAX : INT_MIN);
+		}
+			re = re * 10 + number;
+		}
+			else if (re > 0)
+		{
 
-	at = (*s++ == '-') ? -1 : 1;
+			break;
+		}
+		s++;
 	}
-
-	while (*s >= '0' && *s <= '9')
-{
-	nod = nod * 10 + (*s++ - '0');
-}
-
-	return (at * nod);
+	return (sign * re);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,17 +1,51 @@
 #ifndef MAIN_H
-#define MAIN_h
-
+#define MAIN_H
+/*libreries*/
+#include <unistd.h>
 #include <stdio.h>
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+/*secon proyect libreries*/
+#include <stdlib.h>
+/*prototype*/
+
 int _putchar(char c);
-#endif
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _len(int *s);
+
+int _atoi(char *s);
+
+/*second project prototypes*/
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+#endif /*main.h*/
