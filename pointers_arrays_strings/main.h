3e22a971ff72843b3e6026d36d666debfbbaf955
binbashz <5732@holbertonstudnets.com>
2023-02-28 16:26:55 -0800

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,8 +3,9 @@
 /*libreries*/
 #include <unistd.h>
 #include <stdio.h>
-/*secon proyect libreries*/
+
 #include <stdlib.h>
+
 /*prototype*/
 
 int _putchar(char c);
@@ -33,7 +34,7 @@ int _len(int *s);
 
 int _atoi(char *s);
 
-/*second project prototypes*/
+/*second project prototypes of more pointers arrays*/
 
 char *_strcat(char *dest, char *src);
 
@@ -48,4 +49,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 
 char *cap_string(char *);
+
+char *leet(char *);
+
 #endif /*main.h*/
