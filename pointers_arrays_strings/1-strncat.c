2f5dc3c3cd297d865973b1b620b0a4bc455d896a
binbashz <5732@holbertonstudnets.com>
2023-03-01 05:01:07 -0800

    1-strcat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,43 +1,27 @@
- 
-/**
- * _strncat - concatenates two strings
- * @dest: pointer for the destination string
- * @src: pointer to the source string
- * @n: maximum number of bytes to use from src
- *
- * Return: pointer to the resulting string dest_star
- */
-
-
 #include "main.h"
 #include <stdio.h>
 
-
-	char *_strncat(char *dest, char *src, int n)
+/**
+ * _strncat - concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-	char *dest_start = dest;
+	int dest_len = 0;
+	int i;
 
-    /* Find the end of the destination string */
+	while (dest[dest_len] != '\0')
+		dest_len++;
 
-	while (*dest != '\0')
-{
-	dest++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
 }
 
-	/* Concatenates te origin string */
-
-	int i = 0;
-	while (i < n && *src != '\0')
-{
-	*dest = *src;
- 	dest++;
-	src++;
-	i++;
- }
-
-	/* add null terminator */
-
-	*dest = '\0';
-
-	return (dest_start);
-}
