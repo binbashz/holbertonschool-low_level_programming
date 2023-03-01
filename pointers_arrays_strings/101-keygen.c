a29f0a58069a321d43d4640b4a695a93cf9d3497
binbashz <5732@holbertonstudnets.com>
2023-02-28 16:57:48 -0800

    password-gerenator-srand-rand

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
+	int password(int password_length) {
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+	printf("\t");
+	for(int i = 0; i < password_length; i++) {
+    printf("*");
+}
+	printf("\n");
+	printf("\t");
+	srand(time(NULL));
+	for(int i = 0; i < password_length; i++) {
+    printf("%c", list[rand() % (sizeof list - 1)]);
+}
+	printf("\n");
+	printf("\t");
+	for(int i = 0; i < password_length; i++) {
+    printf("*");
+}
+	printf("\n");
+}
+	int main() {
+	int password_length;
 
-#define LENGTH 8 /* password length*/
-#define CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};':\"\\|,.<>/?`~" /* characters to use in the password*/
-
-int main() {
-    srand(time(NULL)); /* seed the random number generator*/
-
-    char password [LENGTH + 1]; /* password array*/
-
-    /*loop to generate the password*/
-    for (int i = 0; i < LENGTH; i++)
- {
-        password[i] = CHARS[rand() % (sizeof(CHARS) - 1)]; /* select a random character from the character set*/
-    }
-
-    password[LENGTH] = '\0'; /* null-terminate the string */
-
-    printf("Password: %s\n", password); /* print the password*/
-
-    return 0;
+	printf("\n\tEnter length of the password = ");
+	scanf("%d", &password_length);
+	printf("\n");
+	printf("\n");
+	password(password_length);
+	return 0;
 }
