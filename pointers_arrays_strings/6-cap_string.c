188a54ea99cfc0d69c22f21731a84a348fcb92eb
binbashz <5732@holbertonstudnets.com>
2023-03-02 07:09:03 -0800

    update

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to capitalize
+ *
+ * Return: pointer to capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int capitalize;
+
+	capitalize = 0;
+	while (s[capitalize])
+	{
+		if (s[capitalize] >= 'a' && s[capitalize] <= 'z')
+			s[capitalize] -= 'a' - 'A';
+
+		switch (s[capitalize])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '\"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (s[capitalize + 1] >= 'a' && s[capitalize + 1] <= 'z')
+					s[capitalize + 1] -= 'a' - 'A';
+		}
+		capitalize++;
+	}
+	return (s);
+}
