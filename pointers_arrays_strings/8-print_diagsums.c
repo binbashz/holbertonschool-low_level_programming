857188add47e20e77425e36d48f9b3b90855db99
binbashz <5732@holbertonstudnets.com>
2023-03-05 12:43:48 -0800

    main

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,35 +1,26 @@
-
-
-
-
+#include "main.h"
 #include <stdio.h>
 
-/* Definimos la función print_diagsums, que recibe un puntero a un array unidimensional de enteros y el tamaño del lado de la matriz*/
-void print_diagsums(int *a, int size) 
+/**
+ * print_diagsums - prints sum of two diags of a square matrix.
+ * @a: the array.
+ * @size: size of array.
+ */
+void print_diagsums(int *a, int size)
 {
+	int p = 0, sz = size - 1, r1 = 0, r2 = 0;
 
-    int i, j, sum1 = 0, sum2 = 0;  /* Inicializamos los índices i y j, y las variables que almacenarán las sumas de las diagonales*/
+	while (p <= (size * size))
+	{
+		r1 = r1 + a[p];
+		p = p + size + 1;
+	}
 
-    /* Recorremos la matriz para calcular la suma de la diagonal principal y la diagonal secundaria*/
-    for (i = 0; i < size; i++) 
-{
-        for (j = 0; j < size; j++) 
-{
-            if (i == j) 
-{  /* Si estamos en la diagonal principal, sumamos el elemento actual a la suma de la diagonal principal*/
-                sum1 += *(a + i*size + j);
-            }
+	while (sz < (size * size - 1))
+	{
+		r2 += a[sz];
+		sz = sz + size - 1;
+	}
 
-            if (i + j == size - 1) 
-{  /* Si estamos en la diagonal secundaria, sumamos el elemento actual a la suma de la diagonal secundaria*/
-
-                sum2 += *(a + i*size + j);
-            }
-        }
-    }
-
-    /* imprimimos las sumas de las diagonales*/
-
-    printf("main diagonal sum: %d\n", sum1);
-    printf("second diagonal sum: %d\n", sum2);
+	printf("%d, %d\n", r1, r2);
 }
