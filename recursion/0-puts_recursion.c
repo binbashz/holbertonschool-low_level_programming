7931cb5fc00b19ad10a3198b95ce6d5efe804660
binbashz <5732@holbertonstudnets.com>
2023-03-06 05:27:21 -0800

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0') /*  base case: if we reach the end of the string */
 	{
-		_putchar('\n'); /* print a new line character */
+		putchar('\n'); /* print a new line character */
 		return;
 	}
 	putchar(*s); /* print the current character */
