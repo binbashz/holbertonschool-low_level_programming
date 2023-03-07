df31b5e2bad993f7995ca95be3b9f0436f669299
binbashz <5732@holbertonstudnets.com>
2023-03-07 05:52:47 -0800

    6-is_prim-number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,21 +13,44 @@
 
 	int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n <= 1) /* checks if n is less than or equal to 1 */
 {
-	return (0);
+	return (0);    /* returns 0 (not prime) if it is */
 }
 
-	/*  Check if n is divisible by any number up to its square root */
 	int sqrt_n = sqrt(n);
+	/* calculates the square root of n and assigns it to sqrt_n */
 
 	for (int i = 2; i <= sqrt_n; i++)
+		/* loops through all integers from 2 to sqrt_n */
 {
-	if (n % i == 0)
+	if (n % i == 0)  /*  checks if n is divisible by i */
 {
-	return (0);
+	return (0);    /* returns 0 (not prime) if it is */
 	}
 }
 
-	return (1);
+	return (1);    /* returns 1 (prime) if no divisors are found*/
+
+}
+
+/**
+ * main - Entry point fot the program
+ *
+ * Return: Always 0
+ */
+	int main(void)
+{
+	int n = 7;    /* test case*/
+
+	if (is_prime_number(n))
+{
+	printf("%d is prime.\n", n);    /* prints message if n is prime */
+	}
+	else
+	{
+	printf("%d is not prime.\n", n);
+	}
+
+	return (0);
 }
