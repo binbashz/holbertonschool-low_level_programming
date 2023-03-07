b6c2cd8d91b694a214fd97c43c9c3fba16687f8c
binbashz <5732@holbertonstudnets.com>
2023-03-07 15:54:41 -0800

    script_librari-comp

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,3 +1,14 @@
+
 #!/bin/bash
-gcc -c *.c 
-ar -rc  liball.a *.o
+
+# Compile all the .c files in the current directory
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the static library liball.a from the .o files
+ar -rc liball.a *.o
+
+# Index the static library for faster linking
+ranlib liball.a
+
+# Remove the object files
+rm *.o
