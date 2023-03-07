66a2ba95dd6f7183b377fc4634fe9d5cc36314f6
binbashz <5732@holbertonstudnets.com>
2023-03-07 15:56:52 -0800

    scrpit_lib_comp

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,14 +1,3 @@
-
 #!/bin/bash
-
-# Compile all the .c files in the current directory
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Create the static library liball.a from the .o files
-ar -rc liball.a *.o
-
-# Index the static library for faster linking
-ranlib liball.a
-
-# Remove the object files
-rm *.o
+gcc -c *.c
+ar rcs liball.a *o
