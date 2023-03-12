6155d96998f12223da7ae9a3d1b1e63e0b8aef32
binbashz <5732@holbertonstudnets.com>
2023-03-12 16:09:00 -0700

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arguments
+ * @argv: array pointing to the arguments
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	int m, sum = 0;
+
+	if (argc < 1)
+		return (0);
+
+	for (m = 1; m < argc; m++)
+	{
+		if (!atoi(argv[m]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[m]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
