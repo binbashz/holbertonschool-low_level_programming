7a472bc1f19f1c455cb951651c26048025325041
binbashz <5732@holbertonstudnets.com>
2023-03-13 16:00:26 -0700

    0-create-array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,18 +13,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array = NULL;  /* Inicializamos el puntero a NULL*/
-	unsigned int i;     /* Declaramos la variable 'i' de tipo unsigned int*/
+	unsigned int a; /* Declaramos la variable 'A' de tipo unsigned int*/
 
 	if (size == 0)
-	return (NULL);   /* Si el tamaño es 0, devolvemos NULL*/
+	return (NULL); /* Si el tamaño es 0, devolvemos NULL*/
 
 	array = malloc(size * sizeof(char)); /*Asignamos memoria para el array */
 
 	if (array != NULL)  /* Si la asignación de memoria es exitosa:*/
 	{
-	for (i = 0; i < size; i++) /* Iteramos sobre cada elemento del array*/
-	array[i] = c; /* Inicializamos el elemento con el carácter dado*/
+	for (a = 0; a < size; a++) /* Iteramos sobre cada elemento del array*/
+	array[a] = c; /* Inicializamos el elemento con el carácter dado*/
 }
 
 	return (array); /*  Devolvemos el puntero al array.*/
-}
+	}
