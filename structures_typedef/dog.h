41010d5d6b1e7efe92f1f70a69b016b7ec23bcf2
binbashz <5732@holbertonstudnets.com>
2023-03-20 15:19:34 -0700

    struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - struct with name dog.
+ *
+ * @name: name of the dog, first member
+ * @age: age of the dog, second member
+ * @owner: owner of the puppy, thrid member
+ */
+	 struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
