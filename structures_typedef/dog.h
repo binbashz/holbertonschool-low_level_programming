efe730a6640f422e767b2af2980cf52f29e1f4f5
binbashz <5732@holbertonstudnets.com>
2023-03-21 08:24:41 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,10 +13,10 @@
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owneri);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
