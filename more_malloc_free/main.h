d20282b29066d7e6d5f5ec437739f014e1d84014
binbashz <5732@holbertonstudnets.com>
2023-04-05 19:10:18 -0700

    main

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,6 +1,12 @@
 #ifndef __main_h__
 #define __main_h__
 
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
